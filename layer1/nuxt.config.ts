36ec8bbf59a2a4322b462f9ea5885d7cbccdf0b8
Sjoerd van Hout <sjoerd@fysiohier.com>
2026-01-21 17:44:32 +0100

    test nuxt config

--- layer1/nuxt.config.ts
+++ layer1/nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {fileURLToPath} from "node:url";
+
 export default defineNuxtConfig({
     extends: [
         '../base',
     ],
+    alias: {
+        '#layer1': fileURLToPath(new URL('.', import.meta.url)),
+    },
+    routeRules: {
+        /**
+         * PRERENDER SETTINGS
+         */
+        '/_ipx/**': { prerender: true },
+        '/': { prerender: true },
+        '/**': { prerender: true },
+        /**
+         * API SETTINGS
+         */
+        '/api/**': { cors: true },
+    },
+    future: {
+        // https://nuxt.com/blog/v3-10#bundler-module-resolution
+        typescriptBundlerResolution: true,
+    },
 });
