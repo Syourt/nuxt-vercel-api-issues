7667b791b38f328262a85a99dbbdb6281e15b67e
Sjoerd van Hout <sjoerd@fysiohier.com>
2026-01-21 17:09:33 +0100

    test routeRules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    runtimeConfig: {
+        nitro: {
+            envPrefix: 'VERCEL_',
+        },
+        region: process.env.VERCEL_REGION,
+    },
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+        },
+        minify: true,
+        future: {
+            nativeSWR: true,
+        },
+        compressPublicAssets: {
+            gzip: true,
+            brotli: true,
+        },
+    },
+    routeRules: {
+        '/**': { prerender: true },
+    }
 })
